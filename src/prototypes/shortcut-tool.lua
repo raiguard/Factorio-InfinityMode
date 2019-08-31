ff3f8f439c16628611d838365c64af15a5a36b86
raiguard <caleb.heuer@gmail.com>
2019-08-30 21:39:45 -0600

    Add map editor shortcut

--- src/prototypes/shortcut-tool.lua
+++ src/prototypes/shortcut-tool.lua
@@ -7,92 +7,114 @@ local function shortcut_sprite(suffix, size)
         flags = {'icon'}
     }
 end
--- INFINITY SELECTOR
-data:extend{
-    -- selection tool
-    {
-        type = 'selection-tool',
-        name = 'infinity-selector',
-        icons = {{icon='__InfinityMode__/graphics/item/infinity-selector.png', icon_size=64, icon_mipmaps=0}},
-        stack_size = 1,
-        stackable = false,
-        selection_color = infinity_tint,
-        alt_selection_color = infinity_tint,
-        selection_mode = {'any-entity', 'same-force'},
-        alt_selection_mode = {'any-entity'},
-        selection_cursor_box_type = 'electricity',
-        alt_selection_cursor_box_type = 'electricity'
-    },
-    -- shortcut
-    {
-        type = 'shortcut',
-        name = 'infinity-selector',
-        icon = shortcut_sprite('infinity-selector-x32.png', 32),
-        disabled_icon = shortcut_sprite('infinity-selector-x32-white.png', 32),
-        small_icon = shortcut_sprite('infinity-selector-x24.png', 24),
-        disabled_small_icon = shortcut_sprite('infinity-selector-x24-white.png', 24),
-        action = 'create-blueprint-item',
-        item_to_create = 'infinity-selector',
-        associated_control_input = 'im-give-infinity-selector',
-        toggleable = true
-    },
-    -- smoke
-    {
-        type = 'trivial-smoke',
-        name = 'infinity-selector-smoke',
-        animation = {
-			animation_speed = 60 / 40,
-			axially_symmetrical = false,
-			direction_count = 1,
-			filename = '__base__/graphics/entity/smoke/smoke.png',
-			flags = {'smoke'},
-			frame_count = 60,
-			height = 120,
-			line_length = 5,
-			priority = 'high',
-			shift = {-0.53125, -0.4375},
-			width = 152
-        },
-        affected_by_wind = false,
-        color = infinity_tint,
-		cyclic = true,
-		duration = 40,
-		end_scale = 3.0,
-		fade_away_duration = 40,
-		fade_in_duration = 0,
-		flags = {'not-on-map'},
-		spread_duration = 40,
-        start_scale = 0.5
-    },
-    -- custom input
-    {
-        type = 'custom-input',
-        name = 'im-give-infinity-selector',
-        key_sequence = 'CONTROL + SHIFT + S',
-        action = 'create-blueprint-item',
-        item_to_create = 'infinity-selector'
-    }
-    
-}
--- TELEPORTER
+-- MAP EDITOR SHORTCUT
 data:extend{
     -- shortcut
     {
         type = 'shortcut',
-        name = 'im-teleport',
-        icon = shortcut_sprite('teleport-x32.png', 32),
-        disabled_icon = shortcut_sprite('teleport-x32-white.png', 32),
-        small_icon = shortcut_sprite('teleport-x24.png', 24),
-        disabled_small_icon = shortcut_sprite('teleport-x24-white.png', 24),
+        name = 'im-toggle-map-editor',
+        icon = shortcut_sprite('map-editor-x32.png', 32),
+        disabled_icon = shortcut_sprite('map-editor-x32-white.png', 32),
+        small_icon = shortcut_sprite('map-editor-x24.png', 24),
+        disabled_small_icon = shortcut_sprite('map-editor-x24-white.png', 24),
         action = 'lua',
-        associated_control_input = 'im-toggle-teleport-interface',
+        associated_control_input = 'im-toggle-map-editor',
         toggleable = true
     },
     -- custom input
     {
         type = 'custom-input',
-        name = 'im-toggle-teleport-interface',
-        key_sequence = 'CONTROL + SHIFT + T',
+        name = 'im-toggle-map-editor',
+        key_sequence = 'CONTROL + SHIFT + E',
         action = 'lua'
     }
 }
+-- -- INFINITY SELECTOR
+-- data:extend{
+--     -- selection tool
+--     {
+--         type = 'selection-tool',
+--         name = 'infinity-selector',
+--         icons = {{icon='__InfinityMode__/graphics/item/infinity-selector.png', icon_size=64, icon_mipmaps=0}},
+--         stack_size = 1,
+--         stackable = false,
+--         selection_color = infinity_tint,
+--         alt_selection_color = infinity_tint,
+--         selection_mode = {'any-entity', 'same-force'},
+--         alt_selection_mode = {'any-entity'},
+--         selection_cursor_box_type = 'electricity',
+--         alt_selection_cursor_box_type = 'electricity'
+--     },
+--     -- shortcut
+--     {
+--         type = 'shortcut',
+--         name = 'infinity-selector',
+--         icon = shortcut_sprite('infinity-selector-x32.png', 32),
+--         disabled_icon = shortcut_sprite('infinity-selector-x32-white.png', 32),
+--         small_icon = shortcut_sprite('infinity-selector-x24.png', 24),
+--         disabled_small_icon = shortcut_sprite('infinity-selector-x24-white.png', 24),
+--         action = 'create-blueprint-item',
+--         item_to_create = 'infinity-selector',
+--         associated_control_input = 'im-give-infinity-selector',
+--         toggleable = true
+--     },
+--     -- smoke
+--     {
+--         type = 'trivial-smoke',
+--         name = 'infinity-selector-smoke',
+--         animation = {
+-- 			animation_speed = 60 / 40,
+-- 			axially_symmetrical = false,
+-- 			direction_count = 1,
+-- 			filename = '__base__/graphics/entity/smoke/smoke.png',
+-- 			flags = {'smoke'},
+-- 			frame_count = 60,
+-- 			height = 120,
+-- 			line_length = 5,
+-- 			priority = 'high',
+-- 			shift = {-0.53125, -0.4375},
+-- 			width = 152
+--         },
+--         affected_by_wind = false,
+--         color = infinity_tint,
+-- 		cyclic = true,
+-- 		duration = 40,
+-- 		end_scale = 3.0,
+-- 		fade_away_duration = 40,
+-- 		fade_in_duration = 0,
+-- 		flags = {'not-on-map'},
+-- 		spread_duration = 40,
+--         start_scale = 0.5
+--     },
+--     -- custom input
+--     {
+--         type = 'custom-input',
+--         name = 'im-give-infinity-selector',
+--         key_sequence = 'CONTROL + SHIFT + S',
+--         action = 'create-blueprint-item',
+--         item_to_create = 'infinity-selector'
+--     }
+    
+-- }
+-- -- TELEPORTER
+-- data:extend{
+--     -- shortcut
+--     {
+--         type = 'shortcut',
+--         name = 'im-teleport',
+--         icon = shortcut_sprite('teleport-x32.png', 32),
+--         disabled_icon = shortcut_sprite('teleport-x32-white.png', 32),
+--         small_icon = shortcut_sprite('teleport-x24.png', 24),
+--         disabled_small_icon = shortcut_sprite('teleport-x24-white.png', 24),
+--         action = 'lua',
+--         associated_control_input = 'im-toggle-teleport-interface',
+--         toggleable = true
+--     },
+--     -- custom input
+--     {
+--         type = 'custom-input',
+--         name = 'im-toggle-teleport-interface',
+--         key_sequence = 'CONTROL + SHIFT + T',
+--         action = 'lua'
+--     }
+-- }
